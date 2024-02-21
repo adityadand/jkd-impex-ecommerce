282c1bf372ada184ca7b4066ea01fdcdab8d6b49
adityadand <dandaditya@gmail.com>
2024-02-21 17:18:16 +0530

    change mobile view

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,37 @@
 // src/Navbar.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
+const Navbar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/contact">Contact Us</Link></li>
-      <li><Link to="/view-products">View Products</Link></li>
-    </ul>
-  </nav>
-);
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <nav>
+      <div className="navbar-container">
+        <Link to="/" className="logo">JKD IMPEX</Link>
+
+        {/* Burger Menu for Mobile */}
+        <div className={`burger-menu ${isMobileMenuOpen ? 'open' : ''}`} onClick={toggleMobileMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+
+        {/* Navigation Links */}
+        <ul className={`nav-links ${isMobileMenuOpen ? 'open' : ''}`}>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact Us</Link></li>
+          <li><Link to="/view-products">View Products</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
